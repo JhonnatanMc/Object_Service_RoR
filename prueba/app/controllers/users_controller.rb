f488680b09604f4caafdbcee8f3953300f62325d
Jhonnatan <jhonnatan120@hotmail.com>
2015-04-01 08:45:35 -0500

    Correct name
    Change name for ruby standar

--- prueba/app/controllers/users_controller.rb
+++ prueba/app/controllers/users_controller.rb
@@ -31,13 +31,20 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    
-      #@user = UserService.create params[:user]
-      user = UserService.create(params[:user_params])
-    #UserService.create(params[:user_params])
-    
-   
-    # @user = UserService.create(:user)
+         
+      user = UserService.create(user_params)
+
+      respond_to do |format|         
+     
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+      
+    end  
     
   end
   
