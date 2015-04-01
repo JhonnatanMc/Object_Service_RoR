bdf84c51ab7dd7178b94ad0a0d066473064bd66a
Jhonnatan <jhonnatan120@hotmail.com>
2015-04-01 09:49:51 -0500

    config autoload
    Services folder

--- prueba/config/application.rb
+++ prueba/config/application.rb
@@ -11,7 +11,7 @@ module TestApp
   class Application < Rails::Application
     
     
-    #config.autoload_paths += %W( #{config.root}/app/services )
+    config.autoload_paths += %W( #{config.root}/app/services )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
